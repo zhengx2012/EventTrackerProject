6ac939421a51c2ecb5c2181712952ee2e3c915ec
Xian Zheng <xzheng2112@gmail.com>
2018-05-11 16:36:09 -0600

    entity created, gitignore created, README.md

--- TripPlannerJPA/src/com/skilldistillery/planner/entities/Planner.java
+++ TripPlannerJPA/src/com/skilldistillery/planner/entities/Planner.java
@@ -1,8 +1,131 @@
 package com.skilldistillery.planner.entities;
 
+import java.util.Date;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 
 @Entity
 public class Planner {
+	/* attributes */
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+
+	private String city;
+	private String state;
+	private String country;
+
+	@Temporal(TemporalType.DATE)
+	private Date date;
+
+	@Temporal(TemporalType.TIMESTAMP)
+	@Column(name = "flight_time")
+	private Date flightTime;
+
+	@Column(name = "flight_number")
+	private String flightNumber;
+
+	@Column(name = "flight_Company")
+	private String flightCompany;
+
+	@Column(name = "trip_expenses")
+	private double tripExpenses;
+
+	/* constructors */
+	public Planner() {
+	}
+
+	public Planner(int id, String city, String state, String country, Date date, Date flightTime, String flightNumber,
+			String flightCompany, double tripExpenses) {
+		super();
+		this.id = id;
+		this.city = city;
+		this.state = state;
+		this.country = country;
+		this.date = date;
+		this.flightTime = flightTime;
+		this.flightNumber = flightNumber;
+		this.flightCompany = flightCompany;
+		this.tripExpenses = tripExpenses;
+	}
+
+	/* gets and sets */
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public String getCountry() {
+		return country;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+	public Date getFlightTime() {
+		return flightTime;
+	}
+
+	public void setFlightTime(Date flightTime) {
+		this.flightTime = flightTime;
+	}
+
+	public String getFlightNumber() {
+		return flightNumber;
+	}
+
+	public void setFlightNumber(String flightNumber) {
+		this.flightNumber = flightNumber;
+	}
+
+	public String getFlightCompany() {
+		return flightCompany;
+	}
+
+	public void setFlightCompany(String flightCompany) {
+		this.flightCompany = flightCompany;
+	}
+
+	public double getTripExpenses() {
+		return tripExpenses;
+	}
+
+	public void setTripExpenses(double tripExpenses) {
+		this.tripExpenses = tripExpenses;
+	}
 
 }
---
+++ TripPlannerREST/tripplanner.sql
@@ -0,0 +1,57 @@
+-- MySQL Workbench Forward Engineering
+
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
+
+-- -----------------------------------------------------
+-- Schema tripplannerdb
+-- -----------------------------------------------------
+DROP SCHEMA IF EXISTS `tripplannerdb` ;
+
+-- -----------------------------------------------------
+-- Schema tripplannerdb
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `tripplannerdb` DEFAULT CHARACTER SET utf8 ;
+USE `tripplannerdb` ;
+
+-- -----------------------------------------------------
+-- Table `planner`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `planner` ;
+
+CREATE TABLE IF NOT EXISTS `planner` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `city` VARCHAR(45) NOT NULL,
+  `state` VARCHAR(45) NOT NULL,
+  `country` VARCHAR(45) NOT NULL,
+  `date` DATETIME NULL,
+  `flight_time` DATETIME NULL,
+  `flight_number` VARCHAR(45) NULL,
+  `flight_company` VARCHAR(45) NULL,
+  `trip_expenses` DECIMAL(6,2) NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
+
+SET SQL_MODE = '';
+GRANT USAGE ON *.* TO planner@localhost;
+ DROP USER planner@localhost;
+SET SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
+CREATE USER 'planner'@'localhost' IDENTIFIED BY 'goodjob';
+
+GRANT SELECT, INSERT, TRIGGER, UPDATE, DELETE ON TABLE * TO 'planner'@'localhost';
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
+
+-- -----------------------------------------------------
+-- Data for table `planner`
+-- -----------------------------------------------------
+START TRANSACTION;
+USE `tripplannerdb`;
+INSERT INTO `planner` (`id`, `city`, `state`, `country`, `date`, `flight_time`, `flight_number`, `flight_company`, `trip_expenses`) VALUES (1, 'Denver', 'Colorado', 'United States of America', '2018-05-10', '07:00:00', 'G7457', 'Delta', 500.00);
+INSERT INTO `planner` (`id`, `city`, `state`, `country`, `date`, `flight_time`, `flight_number`, `flight_company`, `trip_expenses`) VALUES (2, 'West Palm Beach', 'Florida', 'United States', '2018-05-30', '13:30:00', 'R4579', 'United', 300.00);
+
+COMMIT;
+
