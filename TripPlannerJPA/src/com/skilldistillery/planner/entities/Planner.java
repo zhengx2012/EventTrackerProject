ac0bce02e96e900c69ff43a4f021a58e029ecf9c
Xian Zheng <xzheng2112@gmail.com>
2018-05-11 16:04:43 -0600

    initial commit

---
+++ TripPlannerJPA/src/com/skilldistillery/planner/entities/Planner.java
@@ -0,0 +1,8 @@
+package com.skilldistillery.planner.entities;
+
+import javax.persistence.Entity;
+
+@Entity
+public class Planner {
+
+}
---
+++ TripPlannerREST/src/main/java/com/skilldistillery/planner/TripPlannerRestApplication.java
@@ -0,0 +1,12 @@
+package com.skilldistillery.planner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripPlannerRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TripPlannerRestApplication.class, args);
+	}
+}
